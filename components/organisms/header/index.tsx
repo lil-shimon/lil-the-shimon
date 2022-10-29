cb1ea0a2de08c4a53ac944dc5c6e77e8f53d32ae
Kenta Shimosawa <shimon101018@gmail.com>
2022-10-30 02:58:44 +0900

    feat: 🎸 framer motion

--- components/organisms/header/index.tsx
+++ components/organisms/header/index.tsx
@@ -1,10 +1,23 @@
 import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
 
 export const Header = () => {
   return (
     <>
       <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-        <div className="flex flex-row items-center">
+        <motion.div
+          initial={{
+            x: -500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          className="flex flex-row items-center"
+        >
           <SocialIcon
             url="https://github.com/lil-shimon/"
             fgColor="gray"
@@ -20,7 +33,7 @@ export const Header = () => {
             fgColor="gray"
             bgColor="transparent"
           />
-        </div>
+        </motion.div>
 
         <div className="flex flex-row items-center text-gray-300 cursor-pointer">
           <SocialIcon
