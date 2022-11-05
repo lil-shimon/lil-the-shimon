c014e06d00f68e8853b044046fd9b7341ace0526
Kenta Shimosawa <shimon101018@gmail.com>
2022-11-05 23:42:24 +0900

    feat: 🎸 added buttons

--- components/organisms/hero/index.tsx
+++ components/organisms/hero/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { useTypewriter } from 'react-simple-typewriter'
 import { BackgroundCircles } from '../../atoms/backgroundCircles'
+import Link from 'next/link'
 
 export const Hero: FC = () => {
   const [text, count] = useTypewriter({
@@ -20,6 +21,7 @@ export const Hero: FC = () => {
         className="relative rounded-full h-32 w-32 mx-auto object-cover"
         src="https://avatars.githubusercontent.com/u/69175188?v=4"
       />
+
       <div>
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
           Software Engineer{' '}
@@ -27,6 +29,21 @@ export const Hero: FC = () => {
         <h1 className="text-5xl lg:text-6xl font-semibold scroll-px-10">
           <span className="mr-3">{text}</span>
         </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
       </div>
     </div>
   )
