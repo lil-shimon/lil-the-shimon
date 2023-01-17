80ff099f19020c3f4b093dcdce604211ea7a7d4c
lil-shimon <shimon101018@gmail.com>
2023-01-18 01:39:32 +0900

    feat: 🎸 improve animation

--- components/organisms/about/index.tsx
+++ components/organisms/about/index.tsx
@@ -21,7 +21,13 @@ export const About: FC = () => {
         src={LA_IMAGE}
         className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
       />
-      <div className="space-y-10 px-0 md:px-10">
+      <motion.div
+        initial={{ x: 200 }}
+        transition={{ duration: 2.0, opacity: 0 }}
+        whileInView={{ x: 0 }}
+        viewport={{ once: true }}
+        className="space-y-10 px-0 md:px-10"
+      >
         <h4 className="text-4xl font-semibold">
           Here is a{' '}
           <span className="underline decoration-[#F8AB0A]">little</span>{' '}
@@ -38,7 +44,7 @@ export const About: FC = () => {
           contents here. I am Lil shimon. dummy contents here. I am Lil shimon.
           dummy contents here.
         </p>
-      </div>
+      </motion.div>
     </motion.div>
   )
 }
