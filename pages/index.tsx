e0aa78535c9b66b022a189ae3d972f1442d6dae9
Kenta Shimosawa <shimon101018@gmail.com>
2023-03-11 23:28:54 +0900

    feat: 🎸 tailwind scroolbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import { Skills } from '../components/organisms/skill'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 z-0">
       <Head>
         <title>Lil The Shimon</title>
       </Head>
